d210a7c81d801700d33d1fbf3754f8edbb672407
fjola17 <34029285+fjola17@users.noreply.github.com>
2018-10-11 12:22:46 +0000

    can add two numbers together

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -8,3 +8,7 @@ it("should return return 0 on empty string", () => {
 it("should return the number inputed when there is only one number in the string", () => {
     expect(add("5")).toBe(5);
 });
+
+it("should return sum of two given numbers", () => {
+    expect(add("1,2")).toBe(3);
+});
